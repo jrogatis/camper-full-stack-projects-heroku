a472deb63c560ce27f16f70c497d96761d4f25a9
JEAN P DE ROGATIS <jrogatis@conecta.net.br>
2016-10-11 17:45:14 -0300

    improve on stock market

--- server/api/stocks/stocks-api.controller.js
+++ server/api/stocks/stocks-api.controller.js
@@ -1,6 +1,7 @@
 /**
  * Using Rails-like standard naming convention for endpoints.
  * GET     /api/stocks              ->  index
+ * POST     /api/stocks/quotes       ->  showQuotes
  * POST    /api/stocks              ->  create
  * GET     /api/stocks/:id          ->  show
  * PUT     /api/stocks/:id          ->  upsert
@@ -19,6 +20,7 @@ var _promise = require('babel-runtime/core-js/promise');
 var _promise2 = _interopRequireDefault(_promise);
 
 exports.index = index;
+exports.showQuotes = showQuotes;
 exports.show = show;
 exports.create = create;
 exports.upsert = upsert;
@@ -33,7 +35,105 @@ var _stocksApi = require('./stocks-api.model');
 
 var _stocksApi2 = _interopRequireDefault(_stocksApi);
 
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+var _querystring = require('querystring');
+
+var _querystring2 = _interopRequireDefault(_querystring);
+
+var _requestPromise = require('request-promise');
+
+var _requestPromise2 = _interopRequireDefault(_requestPromise);
+
+var _moment = require('moment');
+
+var _moment2 = _interopRequireDefault(_moment);
+
+var _string = require('string');
+
+var _string2 = _interopRequireDefault(_string);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var dateFormats = ['YYYY-MM-DD', 'MM/DD/YYYY'];
+
+function camelize(text) {
+  return (0, _string2.default)(text).slugify().camelize().s;
+}
+
+function toDate(value, valueForError) {
+  try {
+    var date = (0, _moment2.default)(value, dateFormats).toDate();
+    if (date.getFullYear() < 1400) {
+      return null;
+    }
+    return date;
+  } catch (err) {
+    if (_lodash2.default.isUndefined(valueForError)) {
+      return null;
+    } else {
+      return valueForError;
+    }
+  }
+}
+
+function toFloat(value, valueForNaN) {
+  var result = parseFloat(value);
+  if (isNaN(result)) {
+    if (_lodash2.default.isUndefined(valueForNaN)) {
+      return null;
+    } else {
+      return valueForNaN;
+    }
+  } else {
+    return result;
+  }
+}
+
+function toInt(value, valueForNaN) {
+  var result = parseInt(value, 10);
+  if (isNaN(result)) {
+    if (_lodash2.default.isUndefined(valueForNaN)) {
+      return null;
+    } else {
+      return valueForNaN;
+    }
+  } else {
+    return result;
+  }
+}
+
+function transformHistorical(symbol, data) {
+  var headings = data.shift();
+  return (0, _lodash2.default)(data).reverse().map(function (line) {
+    var result = {};
+    headings.forEach(function (heading, i) {
+      var value = line[i];
+      if (_lodash2.default.includes(['Volume'], heading)) {
+        value = toInt(value, null);
+      } else if (_lodash2.default.includes(['Open', 'High', 'Low', 'Close', 'Adj Close', 'Dividends'], heading)) {
+        value = toFloat(value, null);
+      } else if (_lodash2.default.includes(['Date'], heading)) {
+        value = value;
+        /*value = toDate(value, null);
+        if (value && !moment(value).isValid()) {
+          value = null;
+        }*/
+      }
+      result[camelize(heading)] = value;
+    });
+    result.symbol = symbol;
+    return result;
+  }).value();
+}
+
+function parseCSV(text) {
+  return (0, _string2.default)(text).trim().s.split('\n').map(function (line) {
+    return (0, _string2.default)(line).trim().parseCSV();
+  });
+}
 
 function respondWithResult(res, statusCode) {
   statusCode = statusCode || 200;
@@ -84,9 +184,34 @@ function handleError(res, statusCode) {
   };
 }
 
-// Gets a list of nl
+// Gets a list of stocks
 function index(req, res) {
   return _stocksApi2.default.find().exec().then(respondWithResult(res)).catch(handleError(res));
+}
+
+function showQuotes(req, res) {
+  var values = req.body;
+  var url = 'http://chart.finance.yahoo.com/table.csv?s=';
+  values.from = (0, _moment2.default)(values.from);
+  values.to = (0, _moment2.default)(values.to);
+  var qsa = {
+    s: values.symbol,
+    a: values.from.format('MM') - 1,
+    b: values.from.format('DD'),
+    c: values.from.format('YYYY'),
+    d: values.to.format('MM') - 1,
+    e: values.to.format('DD'),
+    f: values.to.format('YYYY'),
+    g: 'd',
+    ignore: '.csv'
+
+  };
+
+  (0, _requestPromise2.default)({ uri: url, qs: qsa }).then(function (ret) {
+    return parseCSV(ret);
+  }).then(function (data) {
+    return transformHistorical(values.symbol, data);
+  }).then(respondWithResult(res)).catch(handleError(res));
 }
 
 function show(req, res) {
